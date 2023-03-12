29fa32355211519f94cacf3c40644e6d5749524e
Deekuh <90295688+Deekuh@users.noreply.github.com>
2023-03-12 20:36:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+function create_account() {
+    var n=document.getElementById("n1").value;
+    var e=document.getElementById("e1").value;
+    var p=document.getElementById("p1").value;
+    var cp=document.getElementById("p2").value;
+    
+    //Code for password validation
+    var letters=/^[A-Za-z]+$/;
+    var email_val=/^([a-zA-Z0-9])+\@([a-zA-Z0-9])+(\.)+([a-zA-Z0-9])+$/;
+    
+    //Other validation required code
+    if(n=='' || e=='' || p=='' || cp==''){
+        alert("Fields cannot be empty!");
+    }
+    else if(!letters.test(n)){
+        alert('Invalid name format! Must contain alphabets only');
+    }
+    else if(!email_val.test(e)){
+        alert("Invalid email format! Please enter valid email address");
+    }
+    else if(p!=cp){
+        alert("Password does not match!");
+    }
+    else if(document.getElementById("p1").value.length < 8){
+        alert("Password must be at least 8 characters!");
+    }
+    else{
+        alert("Your account has been created successfully!\nRedirecting to login page . . .");
+        window.location="login.html"
+    }
+}
+
+function log_in() {
+    var e=document.getElementById("e1").value;
+    var p=document.getElementById("p1").value;
+    
+    var email_val=/^([a-zA-Z0-9])+\@([a-zA-Z0-9])+(\.)+([a-zA-Z0-9])+$/;
+    
+    if(e=='' || p==''){
+        alert("Fields cannot be empty!");
+    }
+    else if(!email_val.test(e)){
+        alert("Invalid email format! Please enter valid email address");
+    }
+    else{
+        alert("Login successful!");
+    }
+}
